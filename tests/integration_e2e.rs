11ffee3d81a66e0566b5e49dae1ab631cf6e9a3d
Justin Farrell <justinfarrellwebdev@gmail.com>
2026-04-21 22:14:25 -0400

    test(codegen): RED - add target-driven emit_object_file tests
    Add two unit tests that verify emit_object_file accepts a target parameter:
    - emit_object_file_linux_produces_elf: verifies ELF magic bytes for Linux target
    - emit_object_file_windows_msvc_produces_coff: verifies COFF x86_64 machine type for Windows MSVC
    
    Tests currently fail because emit_object_file signature doesn't accept target yet.

--- tests/integration_e2e.rs
+++ tests/integration_e2e.rs
@@ -3,6 +3,7 @@
 use opalescent::compiler::{
     CompileError, compile_program, compile_to_module, emit_object_file, link_object_file,
 };
+use opalescent::build_system::targets::TargetTriple;
 use opalescent::errors::reporter::CompilerError;
 use opalescent::type_system::errors::TypeError;
 use std::fs;
